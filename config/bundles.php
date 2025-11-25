2993aa49abb451474c67a1e17d5be1ee72b647d6
Paul-vincent Santoni <pvs-113@hotmail.fr>
2025-11-25 20:40:59 +0100

    cache cassé mais css presque tout fait

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
+    EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
 ];
